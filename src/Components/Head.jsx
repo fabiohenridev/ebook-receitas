17496dd13a049f7d28caaa907e87b0d7a0515fc6
fabio henrique <henri8274@gmail.com>
2025-03-08 10:16:18 -0300

    update

--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -2,6 +2,9 @@ import "./Head.css";
 export default function Head(){
     return(
         <header className="header">
+            <div className="Promo">
+                <p>PROMOÇÃO VÁLIDA ATÉ DIA 15/03/2025</p>
+            </div>
             <h1>Hobby de cozinha</h1>
         </header>
     )
